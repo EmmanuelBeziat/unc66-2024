6fec1430fda5435dd4752d22a1c2816a747452ef
Emmanuel Béziat <emmanuel@italic.fr>
2024-08-10 04:11:47 +0200

    Update

--- inc/classes/unc66-acf.php
+++ inc/classes/unc66-acf.php
@@ -8,15 +8,19 @@ class UNC66_ACF {
 	 */
 	function __construct () {
 		add_action('acf/init', [$this, 'acf_fields']);
-		// add_action('acf/init', [$this, 'options_page']); // Only if Pro version
+		add_action('acf/init', [$this, 'options_page']); // Only if Pro version
 	}
 
 	/**
 	 * Add ACF options page.
 	 */
 	public function options_page () {
-		if (function_exists('acf_add_options_page')) {
-			acf_add_options_page();
+		if (function_exists('acf_add_options_sub_page')) {
+			acf_add_options_sub_page([
+				'page_title' => 'Options du site',
+        'menu_title' => 'Options du site',
+				'parent_slug' => 'themes.php'
+			]);
 		}
 	}
 
